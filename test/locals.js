7fc2497ec2f38b9792c89e46879905d7f86856ea
James Halliday <substack@gmail.com>
2010-12-20 04:20:23 +0000

    local process list test with mock forever module passes

---
+++ test/locals.js
@@ -0,0 +1,45 @@
+var dnode = require('dnode');
+
+// use the forever in test/mock/ for hackish testing purposes
+require.paths.unshift(__dirname + '/mock');
+var forever = require('forever');
+var telescreen = require(__dirname + '/..');
+
+exports.local_procs = function (assert) {
+    var loadT = setTimeout(function () {
+        assert.fail('telescreen never did forever.load()');
+    }, 100);
+    
+    forever.load = function () {
+        clearTimeout(loadT);
+    };
+    
+    var mock = {};
+    mock.list = [ {
+        pid: 5066,
+        foreverPid: 5065,
+        logFile: '/tmp/forever/foreverlvA.log',
+        options: [],
+        file: 'nexus/index.js',
+        pidFile: '/tmp/forever/pids/foreverlvA.pid',
+    } ];
+    
+    forever.list = function () { return mock.list };
+    
+    var listT = setTimeout(function () {
+        assert.fail('never got the process list');
+    }, 500);
+    
+    var server = telescreen.listen(9999);
+    server.on('ready', function () {
+        dnode.connect(9999, function (remote, conn) {
+            remote.list(function (err, xs) {
+                clearTimeout(listT);
+                if (err) assert.fail(err);
+                assert.eql(xs, mock.list);
+                conn.end();
+                server.end();
+            })
+        }).on('localError', function (err) { throw err });
+    });
+};
