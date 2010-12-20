5be7f258e3daca182057f618b468b0e4aacbd1b5
James Halliday <substack@gmail.com>
2010-12-20 03:23:35 +0000

    untested peering

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var dnode = require('dnode');
 var forever = require('forever');
+var Hash = require('traverse/hash');
+var Seq = require('seq');
 
 var argv = require('optimist').argv;
 var cmd = argv._[0];
@@ -8,9 +10,38 @@ var cmd = argv._[0];
 forever.load();
 
 if (cmd === 'listen') {
+    var peers = {};
+    
     dnode(function (remote, conn) {
+        if (remote.role === 'peer') {
+            peers[remote.name] = remote;
+        }
+        
+        this.local = {
+            list : function (cb) {
+                cb(null, forever.list());
+            },
+        };
+        
         this.list = function (cb) {
-            cb(forever.list());
+            Seq(this)
+                .extend(Hash(peers).values)
+                .parMap(function (peer) {
+                    peer.local.list(this);
+                })
+                .seq(cb)
+                .catch(cb)
+            ;
         };
     }).listen(argv.port || 9999);
+}
+else if (cmd === 'connect') {
+    dnode(function (remote, conn) {
+        this.role = 'peer';
+        this.name = argv.name || process.env.HOSTNAME;
+    }).connect();
+}
+
+function uuid () {
+    return Math.floor(Math.random() * Math.pow(2,32)).toString();
 }
