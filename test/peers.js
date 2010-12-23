e2957ce1ebddcce4acc4c56d307d5314cd4f61c4
James Halliday <substack@gmail.com>
2010-12-23 11:26:28 +0000

    all tests 100% yay

--- test/peers.js
+++ test/peers.js
@@ -1,6 +1,7 @@
 var dnode = require('dnode');
 var fs = require('fs');
 var Seq = require('seq');
+var Hash = require('traverse/hash');
 
 // crafty hack to get around module loading and caching
 var forever = {};
@@ -40,15 +41,12 @@ exports.peer_procs = function (assert) {
         }
     }
     
-    var mock = {};
-    mock.lists = [
-        { abc : genPid() },
-        { def : genPid(), hij : genPid(), klm : genPid() },
-        { nop : genPid() },
-        {}
-    ];
-    mock._lists = mock.lists.slice();
-    forever.list = function () { return mock.lists.shift() };
+    var mock = {
+        peer1 : { abc : genPid() },
+        peer2 : { def : genPid(), hij : genPid(), klm : genPid() },
+        peer3 : { nop : genPid() },
+        server : {}
+    };
     
     var listT = setTimeout(function () {
         assert.fail('never got the process list');
@@ -63,15 +61,15 @@ exports.peer_procs = function (assert) {
                 var next = this;
                 var c = dnode(function () {
                     this.role = 'peer';
-                    this.name = 'peer' + i;
+                    var name = this.name = 'peer' + i;
                     
                     this.local = {};
-                    this.local.list = function (cb) {
-                        cb(null, mock.lists.shift());
+                    this.local.processes = function (cb) {
+                        cb(null, mock[name]);
                     };
                     
-                    this.list = function () {
-                        assert.fail('list called, should be local.list');
+                    this.processes = function () {
+                        assert.fail('directly called, should be local');
                     };
                 });
                 c.connect(port, this.bind({}, null));
@@ -82,11 +80,16 @@ exports.peer_procs = function (assert) {
             })
             .seq(function () {
                 dnode.connect(port, function (remote, conn) {
-                    remote.list(function (err, xs) {
+                    remote.processes(function (err, ps) {
                         clearTimeout(listT);
                         if (err) assert.fail(err);
-                        assert.eql(xs.length, mock._lists.length);
-                        assert.eql(xs.sort(cmp), mock._lists.sort(cmp));
+                        
+                        assert.eql(ps.server, mock.server);
+                        assert.eql(ps.peer1, mock.peer1);
+                        assert.eql(ps.peer2, mock.peer2);
+                        assert.eql(ps.peer3, mock.peer3);
+                        assert.eql(ps, mock);
+                        
                         conn.end();
                         server.end();
                     })
