fa690e56ced9566b3a5bd1535b5e3831453835ae
James Halliday <substack@gmail.com>
2010-12-24 16:36:17 +0000

    restructured to expose telescreen internals

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var forever = require('forever');
 var Hash = require('traverse/hash');
 var Seq = require('seq');
 
-exports = module.exports = server;
-exports.server = server;
-function server (name) {
+exports = module.exports = telescreen;
+function telescreen (name) {
     forever.load();
     
     var peers = {};
@@ -41,66 +40,78 @@ function server (name) {
         });
     }
     
-    return dnode(function (remote, conn) {
-        var self = this;
-        
+    var self = {};
+    
+    self.local = {
+        processes : function (cb) {
+            cb(null, Hash.map(procs, fromProc));
+        },
+        start : function (cmd, options, cb) {
+            if (typeof options === 'function') {
+                cb = options; options = {};
+            }
+            
+            var proc = forever.start(cmd, options || {});
+            
+            do {
+                var id = (Math.random() * Math.pow(2,32)).toString(16);
+            } while (procs[id]);
+            proc.id = id;
+            procs[id] = proc;
+            hookProc(proc);
+            
+            if (cb) cb(null, fromProc(proc));
+        },
+        subscribe : function (emit, cb) {
+            emitters[conn.id] = emit;
+            if (cb) cb(null);
+        },
+    };
+    
+    self.processes = function (cb) {
+        Seq(self)
+            .extend(Hash(peers).values)
+            .parEach(function (peer) {
+                peer.local.processes(this.into(peer.name));
+            })
+            .seq(function () {
+                cb(null, this.vars);
+            })
+           .catch(cb)
+        ;
+    };
+    
+    self.start = function (server, cmd, cb) {
+        peers[server].local.start(cmd, cb);
+    };
+    
+    self.name = name;
+    
+    var service = dnode(function (remote, conn) {
         conn.on('ready', function () {
             if (remote.role === 'peer') {
                 peers[remote.name] = remote;
             }
         });
         
-        self.name = name;
-        self.role = 'peer';
-        
-        self.local = {
-            processes : function (cb) {
-                cb(null, Hash.map(procs, fromProc));
-            },
-            start : function (cmd, options, cb) {
-                if (typeof options === 'function') {
-                    cb = options; options = {};
-                }
-                
-                var proc = forever.start(cmd, options || {});
-                
-                do {
-                    var id = (Math.random() * Math.pow(2,32)).toString(16);
-                } while (procs[id]);
-                proc.id = id;
-                procs[id] = proc;
-                hookProc(proc);
-                
-                if (cb) cb(null, fromProc(proc));
-            },
-            subscribe : function (emit, cb) {
-                emitters[conn.id] = emit;
-                if (cb) cb(null);
-            },
-        };
-        
-        self.processes = function (cb) {
-            Seq(self)
-                .extend(Hash(peers).values)
-                .parEach(function (peer) {
-                    peer.local.processes(this.into(peer.name));
-                })
-                .seq(function () {
-                    cb(null, this.vars);
-                })
-               .catch(cb)
-            ;
-        };
-        
-        self.start = function (server, cmd, cb) {
-            peers[server].local.start(cmd, cb);
-        };
+        return self;
+    });
+    
+    return Hash.merge(self, {
+        listen : function () {
+            self.role = 'server';
+            return service.listen.apply(service, arguments);
+        },
+        connect : function () {
+            self.role = 'peer';
+            return service.connect.apply(service, arguments);
+        }
     });
 };
 
 exports.listen = function () {
-    var server = exports.server('server');
-    return server.listen.apply(server, arguments);
+    var s = telescreen('server', 'server');
+    return s.listen.apply(s, arguments);
 };
 
 exports.fromProc = fromProc;
